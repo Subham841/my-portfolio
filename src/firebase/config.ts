0a890ecf800995954a9c88f09acb27e4d5f42498
Subhamkumar Sahu <subhamkumarsahu207@gmail.com>
2025-11-21 07:03:00 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAUEDBBSaC3xmNl9B3XCql_4cQS_fBiJ-E",
+  authDomain: "studio-2384070340-fd813.firebaseapp.com",
+  projectId: "studio-2384070340-fd813",
+  storageBucket: "studio-2384070340-fd813.appspot.com",
+  messagingSenderId: "59254198116",
+  appId: "1:59254198116:web:c5f672d915ca491226f471",
+  measurementId: ""
 };
