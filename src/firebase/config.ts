41b312667508c7c28f95b2b627a59579215e38d4
Subhamkumar Sahu <subhamkumarsahu207@gmail.com>
2025-11-25 05:16:27 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,8 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyAUEDBBSaC3xmNl9B3XCql_4cQS_fBiJ-E",
-  authDomain: "studio-2384070340-fd813.firebaseapp.com",
-  projectId: "studio-2384070340-fd813",
-  storageBucket: "studio-2384070340-fd813.appspot.com",
-  messagingSenderId: "59254198116",
-  appId: "1:59254198116:web:c5f672d915ca491226f471",
-  measurementId: ""
+  "projectId": "studio-2384070340-fd813",
+  "appId": "1:59254198116:web:c5f672d915ca491226f471",
+  "apiKey": "AIzaSyAUEDBBSaC3xmNl9B3XCql_4cQS_fBiJ-E",
+  "authDomain": "studio-2384070340-fd813.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "59254198116"
 };
